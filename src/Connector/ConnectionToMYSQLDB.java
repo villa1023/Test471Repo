7362340eabcb4f545a587dcfd0a3428e88c4a2a3
bryanmiller7sixty <59464550+bryanmiller7sixty@users.noreply.github.com>
2020-11-13 12:03:04 -0800

    Refactoring code.

--- src/Connector/ConnectionToMYSQLDB.java
+++ src/Connector/ConnectionToMYSQLDB.java
@@ -6,68 +6,31 @@ import java.util.ArrayList;
 public class ConnectionToMYSQLDB {
         public static void main(String[] args)throws Exception{
             getConnection();
-            get();
         }
-        public static String get() throws Exception{
-            try{
-                Connection con = getConnection();
-                PreparedStatement statement = con.prepareStatement("SELECT name,company FROM data");
-                ResultSet result = statement.executeQuery();
-                String str = " ";
-                ArrayList<String> array = new ArrayList<String>();
-                while ((result.next())){
-                    str += "Name: ";
-                    str += result.getString("name");
-                    str += "Company: ";
-                    str += result.getString("company");
-                    //array.add(result.getString("Name"));
-                }
-                return str;
-            }catch(Exception e){
-                System.out.println(e);
-                return null;
+        public static int getTotalNumberRecipes() throws Exception {
+            Connection con = getConnectionToRecipes();
+            PreparedStatement statement = con.prepareStatement("SELECT recipe_id FROM recipe");
+            ResultSet result = statement.executeQuery();
+            int cap = 0;
+            while(result.next()){
+                cap = result.getInt("recipe_id");
             }
+            return cap + 1;
         }
-    public static ArrayList<ArrayList<String>> getTitles() throws Exception{
+    public static ArrayList<ArrayList<String>> getTitles(int count) throws Exception{
         try{
             Connection con = getConnectionToRecipes();
             PreparedStatement statement = con.prepareStatement("SELECT recipe_id,ing_name FROM ingredients");
             ResultSet result = statement.executeQuery();
-            String str = "";
-            int id;
-            ArrayList<ArrayList<String>> list1 = new ArrayList<>();
-            ArrayList<String> list2 = new ArrayList<>();
-            ArrayList<String> list3 = new ArrayList<>();
-            ArrayList<String> list4 = new ArrayList<>();
-            list1.add(list2);
-            list1.add(list3);
-            list1.add(list4);
-
-            while(result.next()){
-                list1.get(result.getInt("recipe_id")).add(result.getString("ing_name"));
+            ArrayList<ArrayList<String>> list = new ArrayList<>();
+            for(int i = 0; i < count; i++){
+                ArrayList<String> list2 = new ArrayList<>();
+                list.add(list2);
             }
-           // int currentIndex = 0;
-//            while (result.next()){
-//                //get corresponding id, this will be the index to which the ingredient will be added, this way each ingredient gets added to it's correct recipe
-//                id = result.getInt("recipe_id");
-//                if(id == (currentIndex)){
-//                    list2.add(result.getString("ing_name"));
-//                    if(!result.next()){
-//                        System.out.println("Adding the final ingredient: " + list2.get(0));
-//                        list1.add(currentIndex, list2);
-//                    }
-//                }else{
-//                    list1.add(currentIndex, list2);
-//                    currentIndex = id;
-////                    for(int i = 0; i < list2.size();i++){
-////                        System.out.println(list2.get(i));
-////                    }
-//                    list2.clear();
-//                    list2.add(result.getString("ing_name"));
-//                }
-//            }
-//            System.out.println("All records have been selected");
-            return list1;
+            while(result.next()){
+                list.get(result.getInt("recipe_id")).add(result.getString("ing_name"));
+            }
+            return list;
         }catch(Exception e){
             System.out.println(e);
             return null;
