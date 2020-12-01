58ad2f5579cbb1ecc78bdf952e3c25105cc9e4ff
bryanmiller7sixty <59464550+bryanmiller7sixty@users.noreply.github.com>
2020-11-30 16:02:26 -0800

    Final product, just needs some clean up and comments.

--- src/Connector/ConnectionToMYSQLDB.java
+++ src/Connector/ConnectionToMYSQLDB.java
@@ -218,7 +218,7 @@ public class ConnectionToMYSQLDB {
             String driver = "com.mysql.jdbc.Driver";
             String url = "jdbc:mysql://localhost:3306/recipes"; //server can be seen as local host followed by the name of the schema
             String username = "root"; //username goes here
-            String password = "Keepyoheadup760!"; //your password goes here
+            String password = ""; //your password goes here
             Connection connect = DriverManager.getConnection(url, username, password);
             System.out.println("Connected!");
             return connect;
